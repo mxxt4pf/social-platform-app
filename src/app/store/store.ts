f820289a49044a22d761acb733b5db7fe9e1e48a
mxxt4pf <mxxt.net.4pf@gmail.com>
2024-10-13 15:40:42 -0600

    created some redux actions

--- src/app/store/store.ts
+++ src/app/store/store.ts
@@ -1,5 +1,5 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { useDispatch, useSelector } from "react-redux";
+import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
 import { testSlice } from "../../features/scratch/testSlice";
 
 export const store = configureStore({
@@ -14,5 +14,5 @@ export type RootState = ReturnType<typeof store.getState>
 export type AppDispatch = typeof store.dispatch
 
 // Use throughout your app instead of plain `useDispatch` and `useSelector`
-export const useAppDispatch = useDispatch.withTypes<AppDispatch>()
-export const useAppSelector = useSelector.withTypes<RootState>()
+export const useAppDispatch: () => AppDispatch = useDispatch
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
--- src/features/scratch/testSlice.ts
+++ src/features/scratch/testSlice.ts
@@ -4,12 +4,24 @@ type State = {
     data: number
 }
 
-const initialState: State ={
+const initialState: State = {
     data: 42
 }
 
 export const testSlice = createSlice({
     name: 'test',
     initialState,
-    reducers: {}
+    reducers: {
+        increment: (state) => {
+            state.data += 1
+        },
+        decrement: (state) => {
+            state.data -= 1
+        },
+        incrementByAmount: (state, action) => {
+            state.data += action.payload
+        }
+    }
 })
+
+export const {increment, decrement, incrementByAmount} = testSlice.actions
