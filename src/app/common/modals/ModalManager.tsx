a52d74ee466e92893a7cdb2c46881d04b8f8a4c2
mxxt4pf <mxxt.net.4pf@gmail.com>
2024-10-18 22:52:36 -0600

    created modal manager

---
+++ src/app/common/modals/ModalManager.tsx
@@ -0,0 +1,22 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import TestModal from "../../../features/scratch/TestModal"
+import { useAppSelector } from "../../store/store"
+
+export default function ModalManager() {
+  const modalLookUp = {
+    TestModal
+  }
+
+  const {type, data, open} = useAppSelector(state => state.modals);
+
+  let renderedModal;
+
+  if(open && type) {
+    const ModalComponent = (modalLookUp as any)[type];
+    renderedModal = <ModalComponent data={data} />
+  }
+  
+    return (
+        <div>{renderedModal}</div>
+  )
+}
