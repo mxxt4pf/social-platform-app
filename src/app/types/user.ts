c6bcf0284ae10152cd2ea2f75fdd553ef006a19f
mxxt4pf <mxxt.net.4pf@gmail.com>
2024-10-27 21:02:41 -0600

    code refactoring

--- src/app/types/user.ts
+++ src/app/types/user.ts
@@ -1,7 +1,7 @@
 export type AppUser = {
-    email: string | null
-    photoUrl: string | null
-    uid?: string
-    displayName: string | null
-    providerID: string | null
+    email: string | null,
+    photoUrl: string | null,
+    uid?: string,
+    displayName: string | null,
+    providerID: string | null,
 }
