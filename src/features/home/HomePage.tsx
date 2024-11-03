83e873b88e6b64337b767df369b8fe6026e64beb
mxxt4pf <mxxt.net.4pf@gmail.com>
2024-11-02 23:42:21 -0600

    refactoring of homepage

--- src/features/home/HomePage.tsx
+++ src/features/home/HomePage.tsx
@@ -6,7 +6,7 @@ export default function HomePage() {
     <Segment inverted textAlign="center" vertical className="masthead">
       <Container>
         <Header as='h1' inverted>
-          <Image size="massive" src="/logo.png" style={{marginBottom: 12}}/>
+          <Image size='massive' src="/logo.png" style={{marginBottom: 12}}/>
           Events Social App
         </Header>
         <Button size='huge' inverted as={Link} to="/events">
